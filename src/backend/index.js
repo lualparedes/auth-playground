286389019ef0b579abe734de6bf8df6f944e728c
Luis Paredes <lualps94@gmail.com>
2019-02-09 19:49:58 -0400

    Refactored backend server

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 
+// Setup
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Routes
+app.get('/', (req, res) => {
+	res.send('Sup bitches?!');
+});
+
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}!`);
+});

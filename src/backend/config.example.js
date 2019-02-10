8d511639970efbb7bb3bf3febb04ef212786d2db
Luis Paredes <lualps94@gmail.com>
2019-02-09 22:15:12 -0400

    Added basic session management

---
+++ src/backend/config.example.js
@@ -0,0 +1,3 @@
+export default {
+  sessionSecret: 'keyboard cat',
+}
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,16 +1,32 @@
 import express from 'express';
+import uuid from 'uuid';
+import session from 'express-session';
+import { default as config } from './config';
 
 // Setup
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(session({
+  genid: (req) => {
+    console.log('Inside session middleware...');
+    console.log(`Your sessionID is: ${req.sessionID}`);
+    return uuid();
+  },
+  secret: config.sessionSecret,
+  resave: false,
+  saveUninitialized: true
+}));
+
 
 // Routes
 app.get('/', (req, res) => {
-	res.send('Sup bitches?!');
+  console.log('Inside route...');
+  console.log(`Your sessionID is: ${req.sessionID}`);
+  res.send(`You hit the home page!\n`);
 });
 
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}!`);
+  console.log(`Listening on port ${port}...`);
 });
